f64a1bcfbe028857a16401ecf8e491a68ac8839d
Isaac Royle <isaac.royle@student.manchester.ac.uk>
2025-10-17 22:24:35 +0100

    Fix piece colours

--- src/main.c
+++ src/main.c
@@ -91,13 +91,13 @@ int main(int argc, char **argv)
     if (!noColoursFlag)
         start_color();
 
-    init_pair(1, COLOR_RED, COLOR_BLACK);
-    init_pair(2, COLOR_GREEN, COLOR_BLACK);
-    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-    init_pair(4, COLOR_BLUE, COLOR_BLACK);
-    init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
-    init_pair(6, COLOR_CYAN, COLOR_BLACK);
-    init_pair(7, COLOR_WHITE, COLOR_BLACK);
+    init_pair(1, COLOR_MAGENTA, COLOR_BLACK); // T
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK); // L - meant to be orange but yellow looks orange
+    init_pair(3, COLOR_BLUE, COLOR_BLACK); // J
+    init_pair(4, COLOR_CYAN, COLOR_BLACK); // I
+    init_pair(5, COLOR_GREEN, COLOR_BLACK); // S
+    init_pair(6, COLOR_RED, COLOR_BLACK); // Z
+    init_pair(7, COLOR_WHITE, COLOR_BLACK); // O - yellow but yellow looks orange
 
     clear();
     // raw();
